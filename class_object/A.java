d54e6dc5c078cbe71d3cfa4307e71fcfe77fd940
Shivanijha14 <shivaniiiijha14@gmail.com>
2025-11-03 22:39:24 +0530

    testing class and method

---
+++ class_object/A.java
@@ -0,0 +1,11 @@
+class A{
+	public static void main(String args[]){
+		int num1 = 9;
+		int num2 = 10;
+		int sum = add(num1, num2);
+		System.out.println("Sum is "+ sum);
+	}
+	static int add(int x, int y){
+		return x+y;
+	}
+}
